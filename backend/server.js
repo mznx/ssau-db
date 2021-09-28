2f7dfcb4c9b7419e0e6533005d765d1ee147a733
mznx <mznx.ux@gmail.com>
2021-09-28 12:20:57 +0400

    [B] Update: basic route structure added to server.js

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,31 @@
-const express = require('express'); //Line 1
-const app = express(); //Line 2
-const port = process.env.PORT || 5000; //Line 3
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
 
 // This displays message that the server running and listening to specified port
-app.listen(port, () => console.log(`Listening on port ${port}`)); //Line 6
+app.listen(port, () => console.log(`Listening on port ${port}`));
 
-// create a GET route
-app.get('/express_backend', (req, res) => { //Line 9
-  res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' }); //Line 10
-}); //Line 11
+// получить рецепты по параметрам
+app.get('/recipes', (req, res) => {
+  let params = req.query;
+  
+  if (!params.hasOwnProperty('categories') && !params.hasOwnProperty('ingredients')) {
+    // select all
+  } else if (!params.hasOwnProperty('categories')) {
+    // select by ingredients
+  } else if (!params.hasOwnProperty('ingredients')) {
+    // select by categories
+  } else {
+    // select by categories and ingredients
+  }
+
+  res.json('response...');
+});
+
+// получить рецепт по id
+app.get('/recipes/:id', (req, res) => {
+  var id = req.params['id'];
+  // select by id
+
+  res.json('response...');
+});
